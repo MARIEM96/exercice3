80b62fd6f9cf3ed7388e7e4f5e05be84091c8eee
MARIEM96 <43937474+MARIEM96@users.noreply.github.com>
2020-08-20 17:16:55 +0100

    Update exercice3.ex

--- lib/exercice3.ex
+++ lib/exercice3.ex
@@ -2,12 +2,22 @@ defmodule Messaging do
   use GenServer
 
 @moduledoc """
-  permet de stocker les messages envoyés dans une liste
-  et les afficher à la demande
+  
   """
 
   @doc """
   ## Examples
+  iex>Messaging.start_link
+  {:ok, #PID<0.174.0>}
+  iex>Messaging.entrer_message("Bonjour")
+  :ok
+  iex>Messaging.afficher_message
+  ["Bonjour"]
+  iex>Messaging.stop  
+  shut down the process
+   ["Bonjour"]
+   does the Genserver exist?
+** (EXIT from #PID<0.158.0>) shell process exited with reason: :ok
 
   """
 
